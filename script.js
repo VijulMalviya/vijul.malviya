c18be2ec41fba623a574564571821d77de4a652f
Vijul Malviya <50556784+VijulMalviya@users.noreply.github.com>
2022-03-13 18:19:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ function scrollFunction() {
     document.getElementById("NavigationBar").style.backgroundColor = "transparent";
   }
 }
+
+function openportfolio(evt, sectionname) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(sectionname).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
